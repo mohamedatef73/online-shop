baeff3de10ea827c55dd4d5c2d5bd176e1c5b5a0
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-11-13 22:29:49 +0200

    create Home screen file with some data

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import HomeScreen from './screens/HomeScreen'
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
