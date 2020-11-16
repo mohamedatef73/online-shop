653c1f8aa744a7e8fde1553c6ade00d056eb3a11
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-11-16 22:34:22 +0200

    creating the data models files from backend server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
 dotenv.config()
 
@@ -24,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000 
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
