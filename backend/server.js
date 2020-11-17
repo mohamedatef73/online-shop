2cd46ee334445e45a75a5a3d8e331d49ca709fc4
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-11-17 16:36:55 +0200

    create product route file install express async handler package and use it to wrap the routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
 import colors from 'colors'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -13,15 +14,8 @@ app.get('/', (req, res) => {
     res.send('welcome to backend side ')
 })
 
-app.get('/api/products', (req, res) => {
-    res.send(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-
-})
 
 const PORT = process.env.PORT || 5000 
 
