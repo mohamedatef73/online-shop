ed44a5d79fa7b4ed9c554223f5113bd9d6606608
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-12-26 23:18:09 +0200

    dealing with user and token data security

---
+++ backend/controllers/userController.js
@@ -0,0 +1,71 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+import generateToken from '../utils/generatorToken.js'
+
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invaild email or password')
+    }
+})
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists ) {
+
+        res.status(400)
+        throw new Error('user already esxists')
+    } 
+
+    const user = await User.create({
+        name,email,password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+
+        })
+    }else{
+        res.status(400)
+        throw new Error('invaild user')
+    }
+})
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('user not found')
+    }
+})
+
+export { authUser, getUserProfile,registerUser }
--- frontend/src/screens/ProducScreen.js
+++ frontend/src/screens/ProducScreen.js
@@ -12,17 +12,31 @@ const ProducScreen = ({ match }) => {
     const dispatch = useDispatch()
 
     const productDetails = useSelector(state => state.productDetails)
-
     const { loading, error, product } = productDetails
 
     useEffect(() => {
         dispatch(ProductDetails(match.params.id))
     }, [dispatch, match])
 
+<<<<<<< Updated upstream
+=======
+    const addToCartHandler = () =>{
+        history.push(`/cart/${match.params.id}?pieces=${pieces}`)
+    }
+
+    let beforeContent = null
+    if(loading){
+        beforeContent = <Loader />
+    }else if (error){
+        beforeContent = <Message variant='danger'>{error}</Message>
+    }
+
+>>>>>>> Stashed changes
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>Back to</Link>
 
+<<<<<<< Updated upstream
             {loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) :
     (
             <Row>
@@ -73,6 +87,84 @@ const ProducScreen = ({ match }) => {
                                 <Button className='btn-block' type='button'
                                     disabled={product.countInStock === 0}>
                                     Add to Cart
+=======
+            {beforeContent?beforeContent :
+                (
+                    <Row>
+                        <Col md={6}>
+                            <Image src={product.image} alt={product.name} fluid />
+
+                        </Col>
+                        <Col md={3}>
+                            <ListGroup variant='flush' >
+                                <ListGroup.Item>
+                                    <h3>{product.name}</h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Rating value={product.rating}
+                                        text={`${product.numReviews} reviews`} />
+                                </ListGroup.Item>
+
+                                <ListGroup.Item>
+                                    Price: ${product.price}
+                                </ListGroup.Item>
+
+                                <ListGroup.Item>
+                                    description {product.description}
+                                </ListGroup.Item>
+                            </ListGroup>
+
+                        </Col>
+                        <Col md={3}>
+                            <Card>
+                                <ListGroup variant='flush'>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Price</Col>
+                                            <Col>
+                                                <strong>${product.price}</strong>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+
+                                    <ListGroup.Item>
+
+                                        <Row>
+                                            <Col>Status</Col>
+                                            <Col>
+                                                {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                                            </Col>
+                                        </Row>
+                                        </ListGroup.Item>
+
+                                   
+                                        <ListGroup.Item>
+                                    {product.countInStock > 0 && (
+                                        
+                                            <Row>
+                                                <Col>Qty</Col>
+                                                <Col>
+
+                                                    <Form.Control as='select' value={pieces} onChange={(e) => setPieces(e.target.value)}>
+                                                       {  [...Array(Number(product.countInStock)).keys()].map(x => (
+                                                            <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                        ))}
+
+
+                                                    </Form.Control>
+                                                </Col>
+                                            </Row>
+                                        
+                                    )}
+                                    </ListGroup.Item>
+
+
+                                    <ListGroup.Item>
+                                        <Button onClick={addToCartHandler}
+                                         className='btn-block' type='button'
+                                            disabled={product.countInStock === '0'}>
+                                            Add to Cart
+>>>>>>> Stashed changes
                         </Button>
                             </ListGroup.Item>
                         </ListGroup>
